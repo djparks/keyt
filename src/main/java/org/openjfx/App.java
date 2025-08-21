5b9fec7b81c0087c3aa7493f1875727f6c13aab6
Darryl Parks <296842+djparks@users.noreply.github.com>
2025-08-21 17:52:40 -0500

    Add menu and help about

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -2,7 +2,13 @@ package org.openjfx;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -17,10 +23,39 @@ public class App extends Application {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
+        // Center content
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        var center = new StackPane(label);
+
+        // Menu bar with Help -> About
+        MenuItem aboutItem = new MenuItem("About");
+        aboutItem.setOnAction(e -> showAboutDialog(stage));
+        Menu helpMenu = new Menu("Help");
+        helpMenu.getItems().add(aboutItem);
+        MenuBar menuBar = new MenuBar(helpMenu);
+
+        // Root layout with menu at top
+        BorderPane root = new BorderPane();
+        root.setTop(menuBar);
+        root.setCenter(center);
+
+        var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void showAboutDialog(Stage owner) {
+        String javaVersion = SystemInfo.javaVersion();
+        String javafxVersion = SystemInfo.javafxVersion();
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setHeaderText("About This Application");
+        alert.setContentText("Java version: " + javaVersion + "\nJavaFX version: " + javafxVersion);
+        alert.getButtonTypes().setAll(ButtonType.CLOSE);
+        if (owner != null) {
+            alert.initOwner(owner);
+        }
+        alert.showAndWait();
     }
 
     public static void main(String[] args) {
